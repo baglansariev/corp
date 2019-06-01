4671c9b9513f9a0a3ae0156a48eeb1ead06a12cd
= <=>
2019-06-01 18:34:51 +0600

    main-slider added

--- application/views/Main/index.php
+++ application/views/Main/index.php
@@ -1,7 +1,7 @@
 <div class="main-slider">
 	<i class="fas fa-chevron-left"></i>
 	<i class="fas fa-chevron-right"></i>
-	<div class="slider-viewport">
+	<div class="slider-viewport" data-current="0">
 		<div class="slider-image">
 			<img src="/public/images/1.jpg" alt="">
 		</div>
@@ -174,9 +174,74 @@
 				</div>
 				<div>
 					<input name="email" type="text">
-					<input type="submit" value="Отправить">
+					<input class="info-btn" type="submit" value="Отправить">
 				</div>
 			</form>
+		</div>
+	</div>
+</div>
+<div class="latest-news container">
+	<h4>Последние новости</h4>
+	<div class="row">
+		<div class="one-news col-xlg-4 col-lg-4">
+			<div class="news-image">
+				<a href="">
+					<img src="/public/images/intro-1.jpg" alt="">
+				</a>
+				<span class="date">
+					01 июн
+				</span>
+			</div>
+			<div class="news-text">
+				<p class="news-heading">
+					<a href="">
+						lorem ipsum dolor sit
+					</a>
+				</p>
+				<p class="news-intro-text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, error, consectetur? Tempora exercitationem
+				</p>
+			</div>
+		</div>
+		<div class="one-news col-xlg-4 col-lg-4">
+			<div class="news-image">
+				<a href="">
+					<img src="/public/images/intro-2.jpg" alt="">
+				</a>
+				<span class="date">
+					01 июн
+				</span>
+			</div>
+			<div class="news-text">
+				<p class="news-heading">
+					<a href="">
+						lorem ipsum dolor sit
+					</a>
+				</p>
+				<p class="news-intro-text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eaque temporibus ipsum dolor
+				</p>
+			</div>
+		</div>
+		<div class="one-news col-xlg-4 col-lg-4">
+			<div class="news-image">
+				<a href="">
+					<img src="/public/images/intro-3.jpg" alt="">
+				</a>
+				<span class="date">
+					01 июн
+				</span>
+			</div>
+			<div class="news-text">
+				<p class="news-heading">
+					<a href="">
+						lorem ipsum dolor sit
+					</a>
+				</p>
+				<p class="news-intro-text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero porro odit officia accusantium
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
--- public/style/js/app.js
+++ public/style/js/app.js
@@ -1,13 +1,44 @@
 $(function(){
 	// Main Slider
 	var sliderWidth = 100;
+	var sliderTimer;
 	var sliderViewportChildCount = $('.slider-viewport').children().length;
 
-	console.log(sliderViewportChildCount);
 	$('.slider-viewport').width(sliderViewportChildCount * sliderWidth + 'vw');
 
+	sliderTimer=setInterval(nextSlide,5000);
+    $('.fa-chevron-right').click(function(){
+        clearInterval(sliderTimer);
+        nextSlide();
+        sliderTimer=setInterval(nextSlide,5000);
+    });
+    $('.fa-chevron-left').click(function(){
+        clearInterval(sliderTimer);
+        prevSlide();
+        sliderTimer=setInterval(nextSlide,5000);
+    });
 
 
+    function nextSlide(){
+	    var currentSlide=parseInt($('.slider-viewport').data('current'));
+	    currentSlide++;
+	    console.log($('.slider-viewport').children().length);
+	    if(currentSlide>=$('.slider-viewport').children().length)
+	    {
+	        currentSlide=0;   
+	    }
+	    $('.slider-viewport').animate({left: -currentSlide*sliderWidth + 'vw'},1000).data('current',currentSlide);
+	}
+
+	function prevSlide(){
+	    var currentSlide=parseInt($('.slider-viewport').data('current'));
+	    currentSlide--;
+	    if(currentSlide<0)
+	    {
+	        currentSlide=$('.slider-viewport').children().length-1;   
+	    }
+	    $('.slider-viewport').animate({left: -currentSlide*sliderWidth + 'vw'},1000).data('current',currentSlide);
+	}
 
 
 
