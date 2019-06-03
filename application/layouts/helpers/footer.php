1107e8d0af4af65a9f55a23cb1cf42f07dcacda4
= <=>
2019-06-03 08:04:30 +0600

    footer parts in processing

--- application/layouts/helpers/footer.php
+++ application/layouts/helpers/footer.php
@@ -2,13 +2,28 @@
 	<div class="footer-top container">
 		<div class="row">
 			<div class="contacts col-lg-4 col-xlg-4">
-				
+				<span>
+					<i class="far fa-envelop"></i>
+				</span>
+				<span>
+					info@company.kz
+				</span>
 			</div>
 			<div class="contacts col-lg-4 col-xlg-4">
-				
+				<span>
+					<i class=""></i>
+				</span>
+				<span>
+					+7 702 111 32 32
+				</span>
 			</div>
 			<div class="contacts col-lg-4 col-xlg-4">
-				
+				<span>
+					<i class=""></i>
+				</span>
+				<span>
+					г.Шымкент, ул.Иляева 23
+				</span>
 			</div>
 		</div>
 	</div>
